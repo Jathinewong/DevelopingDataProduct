d46f4b175ee18482ce0d2ce60680fc5cdf11607a
Jathinewong <jathinewong@hotmail.com>
2016-04-30 22:18:03 -0600

    Changed description

--- ui.R
+++ ui.R
@@ -6,17 +6,15 @@ shinyUI(fluidPage(
 
   titlePanel("Cellphone Subscriptions Prediction by Continent"), 
         
-        h5("This application uses Cellphone subscriptions, food consumption and population density values from 1990 to 2007"),
-        h5("to predict cellphone subscriptions for the year 2008 to 2020."),
-        br(),
+        h5("This application uses Cellphone subscriptions, food consumption and population density from 1990 to 2007 to predict cellphone subscriptions for 2008 to 2020"),
+
         h6("Cellphone Subscriptions per 100 people"),
         h6("Food Consumpton - unit kilocalories available on average, per person, per day."),
         h6("Population density per square km by continent"),
-        br(),
+
         h6(strong("The cellphone subscriptions values show on the graph from the year 1990 to 2007 are the actual values from the World Bank.")),
-        br(),
-        h6("The predicted food consumption and population density data will be calculated by changing the % increase / decrease values, "),
-        h6("and will be apply to the previous year values of each of the continent"),
+
+        h6("The predicted food consumption and population density data will be calculated based on the previous year values with the % increase / decrease."),
 
 
   sidebarLayout(
